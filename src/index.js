a6f5a521ee60e983ccc221090b1950438c877144
recep_hidir <recep.hidir@gmail.com>
2018-02-20 16:40:48 +0300

    Another module imported and useed.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import myModule from './modules/module';
 import materials from './modules/materials';
 import './style.css';
 
-    function component() {
-        let element = document.createElement('div');
+function component() {
+    let element = document.createElement('div');
 
-           // Lodash, now imported by this script
-            element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-            element.classList.add('hello');
+       // Lodash, now imported by this script
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+        element.classList.add('hello');
 
-        return element;
-    }
+    return element;
+}
 
 document.body.appendChild(component());
 console.log(myModule.sayHello("Namee"));
