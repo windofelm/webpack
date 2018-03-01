a9d8003d2aa8b7e5d45e5b030c60fee18122abaf
recep_hidir <recep.hidir@gmail.com>
2018-03-01 12:04:28 +0300

    dotenv(enviroment variables) feature added.

--- sample.js
+++ sample.js
@@ -2,3 +2,7 @@
 require('dotenv').config(); // dotenv
 
 console.log(process.env.DB_USER);
+console.log(process.env.NODE_ENV);
+console.log(process.cwd()); // current working directory of the Node.js process.
+
+process.emitWarning('Something happened!', 'CustomWarning', 'WARN001');
