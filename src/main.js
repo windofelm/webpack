309712421d7db007e42f68dd1a8a84fef37a7b96
recep_hidir <recep.hidir@gmail.com>
2018-05-17 14:24:47 +0300

    Eslint edit.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import _ from 'lodash';
-import {sayHello, sayBay, _isSame} from './modules/module';
+import {sayHello, sayBay, _isSame} from './modules/module'
 
-import isUndefined from './modules/isUndefined';
-import isNull from './modules/isNull';
-import isAlive from './modules/isAlive';
+import isUndefined from './modules/isUndefined'
+import isNull from './modules/isNull'
+import isAlive from './modules/isAlive'
 
-import materials from './modules/materials';
-import './style.css';
+import materials from './modules/materials'
+import './style.css'
 
 function component() {
-    let element = document.createElement('div');
+  let element = document.createElement('div');
 
-       // Lodash, now imported by this script
-        element.innerHTML = _.join(['Hello', 'webpack..'], ' ');
-        element.classList.add('hello');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack..'], ' ');
+  element.classList.add('hello');
 
-    return element;
+  return element
 }
 
 document.body.appendChild(component());
@@ -24,12 +24,12 @@ console.log(materials.sayHello());
 
 
 module.exports = { // Public methods
-    sayHello: sayHello,
-    sayBay: sayBay,
-    isSame: _isSame,
-    isUndefined: isUndefined,
+  sayHello: sayHello,
+  sayBay: sayBay,
+  isSame: _isSame,
+  isUndefined: isUndefined,
     isNull: isNull,
-    isAlive: isAlive
+  isAlive: isAlive
 };
 
 //MyLibrary.sayHello();
